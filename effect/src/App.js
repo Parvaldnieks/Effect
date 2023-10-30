bca2c625a3a33dc3e448d669b850710537c786c1
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-30 09:52:11 +0200

    added

--- effect/src/App.js
+++ effect/src/App.js
@@ -1,21 +1,18 @@
 import ToDo from "./ToDo";
 
-function App() {
 
-  const todo = [{
+function App(props) {
+
+  const todo = {
     userId: 1,
     id: 1,
     title: "An autist has been detected!!!",
     completed: false,
-  }];
-
-  const todojsx = todo.map((user, index) => {
-    return <ToDo key={index} user={user.id} id={user.id} title={user.title} completed={user.completed} />
-  });
+  }
 
   return (
     <>
-    {todojsx}
+    <ToDo {...todo} />
     </>
   );
 }
--- effect/src/ToDo.js
+++ effect/src/ToDo.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 function ToDo(props) {
+
+    const [check, setCheck] = useState(props.completed);
+
+    function handleChange(event) {
+      setCheck(event.target.checked);
+    }
+
     return (
     <>
-        <h1>{props.title}</h1>
-        <input type="checkbox" checked={props.completed}/>
+        <p>UserId: {props.userId} </p>
+        <p>Id: {props.id} </p>
+        <p>{props.title} </p>
+        <label>completed: </label>
+        <input checked={check} onChange={handleChange} type="checkbox" />
     </>
     );
 }
