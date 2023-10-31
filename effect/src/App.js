a77aafbb06305d273a83acc3fdb399c90b870814
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-31 09:51:53 +0200

    added

--- effect/src/App.js
+++ effect/src/App.js
@@ -1,14 +1,24 @@
 import ToDo from "./ToDo";
-
+import { useEffect, useState } from "react";
 
 function App(props) {
 
-  const todo = {
-    userId: 1,
-    id: 1,
-    title: "An autist has been detected!!!",
-    completed: false,
-  }
+  const [todo, setToDo] = useState({});
+  useEffect(() => {
+    async function getToDo() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/4');
+      const data = await response.json();
+      setToDo(data);
+      }
+    getToDo();
+  }, []);
+  
+  // const todo = {
+  //   userId: 1,
+  //   id: 1,
+  //   title: "An autist has been detected!!!",
+  //   completed: false,
+  // }
 
   return (
     <>
